a24d799f378646fb5ef7927105dfaaf558f62e41
Nils <its.just.nils@gmail.com>
2020-06-10 10:17:07 +0200

    remove obsolete code

--- Assets/DungeonCrawler/Scripts/EntityComponents/Components/Views/TileViewComponent.cs
+++ Assets/DungeonCrawler/Scripts/EntityComponents/Components/Views/TileViewComponent.cs
@@ -15,40 +15,7 @@ namespace DungeonCrawler.EntityComponents.Components
         {
             base.OnStart();
 
-            LoadFinishedEvent += OnLoadFinished;
-
             tileDataComponent = GetComponent<TileDataComponent>(true);
-        }
-
-        protected override void OnStop()
-        {
-            base.OnStop();
-
-            LoadFinishedEvent -= OnLoadFinished;
-        }
-
-        private void OnLoadFinished(ViewComponent viewComponent)
-        {
-            // SetMaterial();
-        }
-
-        private void SetMaterial()
-        {
-            if (tileDataComponent.Data.tileState != Levels.TileState.Doorway)
-            {
-                return;
-            }
-
-            AsyncOperationHandle<Material> handle = Addressables.LoadAssetAsync<Material>(doorwayMaterialKey);
-            handle.Completed += OnLoadPrefabCompleted;
-        }
-
-        private void OnLoadPrefabCompleted(AsyncOperationHandle<Material> handle)
-        {
-            Addressables.Release(handle);
-            handle.Completed -= OnLoadPrefabCompleted;
-
-            gameObject.GetComponent<Renderer>().material = handle.Result;
         }
 
         public override void Load()
